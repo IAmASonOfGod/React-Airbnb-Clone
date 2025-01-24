d3e7db0c7375fa268f7819426174bd2f0a93730a
Lesego Ratau <rataulesego95@gmail.com>
2025-01-25 00:13:21 +0200

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors"); 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-// Set up default middlewares (logger, static, cors, no-cache)
+
+server.use(cors({ origin: "https://majestic-macaron-bc4906.netlify.app" }));
+
 server.use(middlewares);
 
-// Set up authentication middleware
+
 server.db = router.db;
 server.use(auth);
+
 server.use(router);
 
-// Listen on port 5000
-server.listen(5000, () => {
-  console.log("JSON Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
